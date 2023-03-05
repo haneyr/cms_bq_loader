963b971b9fbb887f768c41d4be393ac329155323
haneyr <67476265+haneyr@users.noreply.github.com>
2023-03-05 14:17:40 -0500

    Updated batch_processor

--- batch_processor.py
+++ batch_processor.py
@@ -3,6 +3,7 @@ import ijson
 import requests
 import gzip
 import shutil
+import gcsfs
 
 from google.cloud import storage
 from google.cloud import bigquery
@@ -11,17 +12,22 @@ from threading import Thread
 from queue import Queue
 from urllib.parse import urlparse
 
-def file_download(file_url,gcsfs_path,downloadKey,filename):
+project_id = "cms-transparency-project"
+fs = gcsfs.GCSFileSystem(project=project_id)
+
+def file_download(file_url,bucket_name,downloadKey,filename):
     response = requests.get(url=file_url,allow_redirects=True,).content
-    with open(f"{gcsfs_path}/{filename}",'wb') as out:
+    with fs.open(f"{bucket_name}/{downloadKey}/{filename}",'wb') as out:
         out.write(response)
 
-def gunzip_file(gcsfs_path,filename):
-    with gzip.open(f"{gcsfs_path}/{filename}",'rb') as f_in:
+def gunzip_file(bucket_name,downloadKey,filename):
+    with fs.open(f"{bucket_name}/{downloadKey}/{filename}",'rb') as f_in:
         output_file = os.path.splitext(filename)[0]
-        with open(f"{gcsfs_path}/{output_file}",'wb') as f_out:
-            shutil.copyfileobj(f_in, f_out)
-    return f"{gcsfs_path}/{output_file}"
+        #g = gzip.GzipFile(fileobj=f_in)
+        with fs.open(f"{bucket_name}/{downloadKey}/{output_file}",'wb') as f_out:
+            print(f"Splitter input file: {bucket_name}/{downloadKey}/{output_file}")
+            shutil.copyfileobj(gzip.GzipFile(fileobj=f_in), f_out)
+    return f"{bucket_name}/{downloadKey}/{output_file}"
 
 def parse_filename(url):
     parsedUrl = urlparse(url)
@@ -30,17 +36,17 @@ def parse_filename(url):
 def load_json_to_bq(table_id, bucket_uri):
     client = bigquery.Client()
     job_config = bigquery.LoadJobConfig(
-    autodetect=True, source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+    autodetect=True, source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON, max_bad_records=100000
     )
     uri = f"gs://{bucket_uri}*.json"
     load_job = client.load_table_from_uri(
     uri, table_id, job_config=job_config
-    )  # Make an API request.
-    load_job.result()  # Waits for the job to complete.
+    )  
+    load_job.result()  
     destination_table = client.get_table(table_id)
-    print("Loaded {} rows.".format(destination_table.num_rows))
+    print("Loaded {} rows to {}.".format(destination_table.num_rows, destination_table))
 
-def producer(queue, source_file_name):
+def producer(queue,splitter_input_file):
     record_buffer = []
     json_target_items = 'in_network.item'
     counter = 0
@@ -51,12 +57,11 @@ def producer(queue, source_file_name):
     print('setting backend')
     backend = ijson.get_backend('yajl2_c')
     print("opening file")
-    with open(source_file_name, 'rb') as data:
+    with fs.open(f"{splitter_input_file}",'rb') as data:
         print('starting to iterate items')
         for obj in backend.items(data, json_target_items,use_float=True,buf_size=file_input_buffer):
             counter +=  1
             record_buffer.append(str(obj))
-
             if counter == buffer_size:
                 print(f"adding record array to queue.")
                 queue.put((file_part_index, record_buffer))
@@ -64,7 +69,6 @@ def producer(queue, source_file_name):
                 record_buffer = []
                 file_part_index += 1
                 counter = 0
-
     if counter > 0:
         print('Flushing remaining buffer records.')
         queue.put((file_part_index, record_buffer))
@@ -102,29 +106,27 @@ def consumer(queue, output_path, target_file_name, worker_id, bucket_name):
         print(f'chunk processed by worker {worker_id}. File Created: {output_path}{target_file_name}_{chunk[0]}.json')
 
 def main():
-    #file_url = "https://uhc-tic-mrf.azureedge.net/public-mrf/2023-02-01/2023-02-01_ALL-SAVERS-INSURANCE-COMPANY_Insurer_PPO---NDC_PPO-NDC_in-network-rates.json.gz"
-    #Get the URL env var passed to the batch job
-    file_url = os.environ.get('URL')
+    #Get the URL env var passed to the batch job based on the task index
+    #Batch invoker script passes a dict with {'URL<Index Number>':'https://...'} to every task
+    batch_index_id = os.environ.get('BATCH_TASK_INDEX')
+    file_url = os.environ.get(f"URL{batch_index_id}")
     filename = parse_filename(file_url)
     tablename = os.path.splitext(os.path.splitext(filename)[0])[0]
-    gcsfs_path = "/mnt/disks/bucket/"
-    project_id = "cms-transparency-project"
-    region = "us-central1"
     bq_dataset = "my_dataset"
     bucket_name = "cms-json-trigger-bucket"
     downloadKey = f"{tablename}/downloaded"
-    num_file_writers = 20 #  5for small 70 for big
+    num_file_writers = 50 #  5 for small 70 for big
     queue_size = 1500 # -1 for unlimited
     queue = Queue(queue_size)
-    queue_saturation_time = 200  # in seconds 60 for small 1800 for big
-    input_filename = '/mnt/disks/bucket/2023-02-01_ALL-SAVERS-INSURANCE-COMPANY_Insurer_PPO---NDC_PPO-NDC_in-network-rates.json'
+    queue_saturation_time = 60  # in seconds 60 for small 1800 for big
     output_path = f"{tablename}/load_to_bq/"
     output_filename = "in-network-rates" 
     print(f"Streaming {filename} from {file_url} to {downloadKey}")
-    file_download(file_url,gcsfs_path,downloadKey,filename)
-    output_file = gunzip_file(gcsfs_path,filename)
+    file_download(file_url,bucket_name,downloadKey,filename)
+    splitter_input_file = gunzip_file(bucket_name,downloadKey,filename)
+    print(splitter_input_file)
     print('Starting file splitter...')    
-    p = Thread(target=producer, args=(queue,input_filename,))
+    p = Thread(target=producer, args=(queue,splitter_input_file))
     p.start()
     
     print(f'Pausing for {queue_saturation_time/60} minutes to allow queue to saturate...')
